e1d34ee5f3cef6d40a574345d987a8ecf6bcdbed
Juan David Rivera <60453003+Juan9507@users.noreply.github.com>
2023-01-11 10:52:01 -0500

    Create parametrizadoDSL3.groovy

---
+++ parametrizadoDSL3.groovy
@@ -0,0 +1,23 @@
+job('ejemplo2-job-DSL') {
+  description('Job DSL de ejemplo para el curso de jenkins')
+  scm {
+    git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
+      node / gitConfigName('Juan9507')
+      node / gitConfigEmail('juandavidnaranjo75@gmail.com')
+    }
+  }
+  parameters {
+  	stringParam('nombre', defaultValue = 'Juan', description = 'Parametro de cadena para el Job Booleano')
+    choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+    booleanParam('agente', false)
+  }
+  triggers {
+    cron('H/7 * * * *')
+  }
+  steps {
+    shell("bash jobscript.sh")
+  }
+  publishers {
+    mailer('jdrivera891@misena.edu.co', true, true)
+  }
+}
